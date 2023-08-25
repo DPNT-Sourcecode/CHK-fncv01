caa6454346a001860261b487f615017a2b90d15c
sbx_user1051 <sbx_user1051@169.254.189.77>
2023-08-25 12:49:13 +0000

    Fri Aug 25 12:49:13 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,7 +1,23 @@
 package solutions.CHK
 
+/**
+ * +------+-------+----------------+
+ * | Item | Price | Special offers |
+ * +------+-------+----------------+
+ * | A    | 50    | 3A for 130     |
+ * | B    | 30    | 2B for 45      |
+ * | C    | 20    |                |
+ * | D    | 15    |                |
+ * +------+-------+----------------+
+ */
 object CheckoutSolution {
+    val pricesPerSKU = mapOf(
+        "A" to 50,
+        "B" to 30,
+        "C" to 20,
+        "D" to 15
+    )
     fun checkout(skus: String): Int {
-        TODO("Solution not implemented")
+
     }
 }
